08be889caa4e3593e5d8ab725b8a9c19873e238e
Real <swrely@qq.com>
2022-11-02 00:09:34 +0800

    feat: add cors config

---
+++ mall-gateway/src/main/java/com/company/mallgateway/config/GlobalCorsConfiguration.java
@@ -0,0 +1,30 @@
+package com.company.mallgateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+/**
+ * @author Real
+ * Date: 2022/11/1 23:37
+ */
+@Configuration
+public class GlobalCorsConfiguration {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        // 1、配置跨域参数
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.setAllowCredentials(true);
+        // 2、注册跨域配置
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsWebFilter(source);
+    }
+
+}
