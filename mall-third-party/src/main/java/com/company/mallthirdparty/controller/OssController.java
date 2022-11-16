85ec44d96b1545dec89bc7faf4fa4739d7c94350
Real <swrely@qq.com>
2022-11-17 01:22:38 +0800

    fix: fix third-party dir

--- mall-third-party/src/main/java/com/company/mallthirdparty/controller/OssController.java
+++ mall-third-party/src/main/java/com/company/mallthirdparty/controller/OssController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -39,13 +40,13 @@ public class OssController {
      */
     @RequestMapping("/oss/policy")
     public R policy() {
-
         // 填写Host地址，格式为https://bucketname.endpoint。
         String host = "https://" + bucket + "." + endpoint;
         // 设置上传回调URL，即回调服务器地址，用于处理应用服务器与OSS之间的通信。OSS会在文件上传完成后，把文件上传信息通过此回调URL发送给应用服务器。
         // String callbackUrl = "https://192.168.0.0:8888";
         // 设置上传到OSS文件的前缀，可置空此项。置空后，文件将上传至Bucket的根目录下。
-        String dir = "jpg/";
+        String date = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+        String dir = date + "/";
         Map<String, String> respMap = new LinkedHashMap<>(8);
         try {
             long expireTime = 30;
