78e7e48e6b5e9c1069c3f0e5d969149d5cde8853
Real <swrely@qq.com>
2022-11-24 00:15:53 +0800

    feat: add attr group search

--- mall-product/src/main/java/com/company/mallproduct/service/impl/AttrGroupServiceImpl.java
+++ mall-product/src/main/java/com/company/mallproduct/service/impl/AttrGroupServiceImpl.java
@@ -5,6 +5,7 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.company.mallcommon.utils.PageUtils;
 import com.company.mallcommon.utils.Query;
@@ -31,20 +32,21 @@ public class AttrGroupServiceImpl extends ServiceImpl<AttrGroupDao, AttrGroupEnt
 
     @Override
     public PageUtils queryPage(Map<String, Object> params, Long catelogId) {
-        // 为 0 查出所有的分类信息
-        if (catelogId == 0) {
-            IPage<AttrGroupEntity> page = this.page(new Query<AttrGroupEntity>().getPage(params), new QueryWrapper<AttrGroupEntity>());
-            return new PageUtils(page);
-        }
         // 使用Key关键字检索关键字内容
         String key = (String) params.get("key");
         // select * from pms_attr_group where catelog_id = ? and (attr_group_id = key or attr_group_name like '%key%')
-        LambdaQueryWrapper<AttrGroupEntity> wrapper = Wrappers.<AttrGroupEntity>lambdaQuery().eq(AttrGroupEntity::getCatelogId, catelogId);
+        LambdaQueryWrapper<AttrGroupEntity> wrapper = Wrappers.<AttrGroupEntity>lambdaQuery();
         if (StringUtils.isNotBlank(key)) {
             // 添加构造条件
-            wrapper.and(queryWrapper -> queryWrapper.eq(AttrGroupEntity::getAttrGroupId, key).or().like(AttrGroupEntity::getAttrGroupName, key));
+            wrapper.eq(AttrGroupEntity::getAttrGroupId, key).or().like(AttrGroupEntity::getAttrGroupName, key);
         }
-        IPage<AttrGroupEntity> page = this.page(new Query<AttrGroupEntity>().getPage(params), wrapper);
+        IPage<AttrGroupEntity> page = new Page<>();
+        // 为 0 查出所有的分类信息
+        if (catelogId == 0) {
+            page = this.page(new Query<AttrGroupEntity>().getPage(params), wrapper);
+            return new PageUtils(page);
+        }
+        page = this.page(new Query<AttrGroupEntity>().getPage(params), wrapper.eq(AttrGroupEntity::getCatelogId, catelogId));
         return new PageUtils(page);
     }
 
